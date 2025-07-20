e5b7af3565e19a03d1b99fb3028550b856b2c6f9
NawafAldhawi <nawafuni2020@gmail.com>
2025-07-20 05:53:19 +0300

    ypieee

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,13 @@ export default function Home() {
   const [Goal1, setGoal1] = useState(false);
   const [Goal2, setGoal2] = useState(false);
   const [Goal3, setGoal3] = useState(false);
+  const [GetStarted, setGetStarted] = useState(false);
 
   return (
+
     <div className="min-h-screen flex flex-col bg-white">
+      
+    <div className="scale-[0.9] origin-top-left w-[111.11%] h-[111.11%]"> 
       {/* Sticky Header */}
       <div  className="sticky top-0 z-30 bg-gray-100 shadow-md">
         <div className="h-[80px] bg-gray-100 flex items-center px-5 shadow-md z-10 sticky top-0">
@@ -18,7 +22,7 @@ export default function Home() {
           <div className="ml-auto mr-10 hidden md:block">
             <button
               onClick={() => setIsOpen(!isOpen)}
-              className="bg-orange-500 text-white px-4 py-2 rounded-lg font-semibold"
+              className="bg-orange-500 text-white px-4 py-2 rounded-lg font-semibold  hover:scale-105"
             >
               Menu
             </button>
@@ -54,11 +58,30 @@ export default function Home() {
         <div className="flex min-h-screen">
 
           {/* LEFT: Image Section */}
-          <div className="flex-[3] h-screen">
-            <section className="h-screen w-full">
-              <img src="/stockbs.jpg" alt="Image 1" className="object-cover w-full h-[120%]" />
-            </section>
+          <div className="relative w-full aspect-[16/9]"> {/* maintains ratio across zoom */}
+            <img
+              src="/stockbs.jpg"
+              alt="Image 1"
+              className="absolute inset-0 object-cover w-full h-full"
+            />
+            <div className="absolute bottom-0 w-full h-1/2">
+            <div className="absolute bottom-0 w-full h-full bg-white opacity-70" />
+            <button className="absolute rounded-full bottom-[20%] left-[15%] w-[20rem] h-[20rem] bg-orange-500 overflow-hidden text-9xl font-bold opacity-100 z-10
+            duration-900 hover:scale-105"
+            onMouseEnter={() => setGetStarted(true)} onMouseLeave={() => setGetStarted(false)}
+                >   <span className="text-[10rem] absolute top-[25%] left-[24%] leading-none">➜</span>
+                  </button>
+             
+             <div className ={`absolute bottom-[20%] left-[15%] w-[20rem] h-[20rem] relative `}>
+              <h1 className={`absolute text-7xl whitespace-nowrap top-65 text-orange-500 font-bold font-serif transition-all
+                ${GetStarted ? 'duration-1000 opacity-100 translate-x-[90%] ' : ' translate-x-0  opacity-0'}`
+              }> Get Started</h1>
+             </div>
+            </div>
+
+            
           </div>
+
 
           {/* RIGHT: ABOUT US Section */}
           <div className="flex-[0.1] min-h-screen flex flex-col items-center relative overflow-visible">
@@ -94,32 +117,37 @@ export default function Home() {
             <div className="mt-3 px-8 py-6 max-w-[90%] flex justify-center rounded-lg gap-6 text-orange-500 relative leading-relaxed ">
 
               {/* im a genius */}
+              
+              {/* TUNRS OUT YOU CAN DO THE HOVER ANIMATION JUST WITH "hover:scale-105" AND "group-hover" I AM RETARDED? IDK */}
 
               {/* normal button {Innovation} */}
               <button onMouseEnter={() => setGoal1(true)} onMouseLeave={()=>setGoal1(false)}
-              className={`outline outline-3 outline-orange-500 text-orange-600 px-10 py-40 rounded-lg font-bold font-serif text-3xl relative z-10 
-              ${!Goal1 ? 'pr-10 py-10 opacity-100' : 'max-h-0 p-0 opacity-0'} `}>
+              className={`outline outline-3 outline-orange-500 text-orange-600 px-10 py-40 transition-all   rounded-lg font-bold font-serif text-3xl relative z-10 
+              ${!Goal1 ? 'duration-[1100ms] pr-10 py-10 opacity-100' : 'duration-[0ms] max-h-0 p-0 opacity-0'} `}>
                 Innovation
               </button>     
 
               {/* Hover animation button {Innovation} */}
               <button 
-              className={`absolute left-0 bg-orange-500 text-white px-10 py-40  overflow-hidden rounded-lg font-bold font-serif text-3xl z-0
-              ${Goal1 ? 'pr-10 py-10 opacity-100' : 'max-h-0 p-0 opacity-0'}`}>
+              className={`absolute right-123 bg-orange-500 text-white px-12 py-41 transition-all  duration-700 ease-in-out overflow-hidden rounded-lg font-bold font-serif text-3xl z-0
+              ${Goal1 ? 'pr-10 pl-10 w-[36%] h-[93%] bottom-5 opacity-100 shadow-2xl' : 'max-h-0 p-0 opacity-0'}`}>
                 Innovation
-              </button>
+              </button> 
+                {/* i also made it get a little bigger on hover with transition animation with the w-[34%] h-[93%] and bottom-5) */}
+
+
 
               {/* normal button {Growth} */}
               <button onMouseEnter={()=> setGoal2(true)} onMouseLeave={()=>setGoal2(false)}
-              className={`outline outline-3 outline-orange-500 text-orange-600 px-10 py-40 rounded-lg font-bold font-serif text-3xl  relative z-10
-              ${!Goal2 ? 'pr-10 py-10 opacity-100' : 'max-h-0 p-0 opacity-0'}`}> 
+              className={`outline outline-3 outline-orange-500 text-orange-600 px-10 py-40 rounded-lg transtion-all font-bold font-serif text-3xl  relative z-10
+              ${!Goal2 ? 'duration-[1100ms] pr-10 py-10 opacity-100' : 'duration-[0ms] max-h-0 p-0 opacity-0'}`}> 
                 Growth
               </button>   
 
               {/* Hover animation button {Growth} */}
               <button 
-              className={`absolute left-68.5 bg-orange-500 text-white px-10 py-40 rleative  overflow-hidden rounded-lg font-bold font-serif text-3xl z-0
-              ${Goal2 ? 'pr-10 py-10 opacity-100' : 'max-h-0 p-0 opacity-0'}`}>
+              className={`absolute left-64.5 bg-orange-500 text-white px-10 py-40 rleative duration-700 ease-in-out   overflow-hidden rounded-lg font-bold font-serif text-3xl z-0
+              ${Goal2 ? 'py-10 w-[29.5%] h-[96%] mr-30 bottom-3 opacity-100 shadow-2xl' : 'max-h-0 p-0 opacity-0'}`}>
                 Growth
               </button>
 
@@ -133,31 +161,11 @@ export default function Home() {
               {/* Hover animation button {Community} */}   
               <button 
               className={`absolute right-0 bg-orange-500 text-white px-10 py-40 rleative  overflow-hidden rounded-lg font-bold font-serif text-3xl z-0
-              ${Goal3 ? 'pr-10 py-10 opacity-100' : 'max-h-0 p-0 opacity-0'}`}>
+              ${Goal3 ? 'pr-10 py-10 opacity-100 shadow-2xl' : 'max-h-0 p-0 opacity-0'}`}>
                 Community
               </button>
             </div>
 
-            {/* 
-            <div className={`mt-3 px-8 py-6 max-w-[90%] flex justify-center ounded-lg gap-6 text-orange-500 leading-relaxed ${
-              Goal1 ? 'max-h-[200px] pr-20 p-5' : 'max-h-0 p-0'
-            }`} >
-
-              <button
-              className="outline outline-3 outline-orange-500 text-orange px-10 py-40 rounded-lg font-bold font-serif text-3xl">
-                Innovation
-              </button>     
-
-              <button
-              className="outline outline-3 outline-orange-500 text-orange px-10 py-40 rounded-lg font-bold font-serif text-3xl">
-                Growth
-              </button>   
-
-              <button  className="outline outline-3 outline-orange-500 text-orange px-10 py-40 rounded-lg font-bold font-serif text-3xl">
-                Community 
-              </button>     
-            </div>
-            */}
 
           </div>
         </div>
@@ -170,6 +178,7 @@ export default function Home() {
           <img src="/image.webp" alt="Image 3" className="object-cover w-full h-auto" />
         </section>
       </main>
+    </div>
     </div>
   );
 }
